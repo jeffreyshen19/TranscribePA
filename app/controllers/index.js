1ca26d9d8c182c43a5f98ac7b72b71a638c66bb3
jeffreyshen19 <jshen@andover.edu>
2019-01-23 00:39:23 -0500

    add code to get random document for transcription

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,7 +2,7 @@ var express = require('express'),
   router = express.Router();
 
 //Import routes
-// router.use('/transcribe', require('./transcribe'));
+router.use('/transcribe', require('./transcribe'));
 router.use('/admin', require('./admin'));
 // router.use('/verify', require('./verify'));
 router.use('/browse', require('./browse'));
--- app/controllers/transcribe.js
+++ app/controllers/transcribe.js
@@ -1,4 +1,23 @@
 var express = require('express'),
   router = express.Router();
+var Document = require("../models/Document");
+
+// Get a random document to transcribe based upon designated search parameters
+router.get('/', function(req, res){
+  var filter = {
+    transcribed: false
+  };
+  
+  Document.count(filter).exec(function (err, count) {
+    var random = Math.floor(Math.random() * count);
+
+    Document.findOne(filter).skip(random).exec(
+      function (err, result) {
+        res.render("transcribe", {
+          document: result
+        });
+      });
+  });
+});
 
 module.exports = router;
