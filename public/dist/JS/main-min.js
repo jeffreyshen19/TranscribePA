3bc6c729fb36ecc6ddb3173f1d3e32e291ef5404
jeffreyshen19 <jshen@andover.edu>
2019-02-05 23:20:44 -0500

    partially finished index

---
+++ public/dist/JS/main-min.js
@@ -0,0 +1,1 @@
+var typed=new Typed("#header-text",{strings:["transcribe documents","digitize our collections","improve ","help researchers"],typeSpeed:30});
---
+++ public/src/JS/main.js
@@ -0,0 +1,5 @@
+// Animate home page typing
+var typed = new Typed('#header-text', {
+  strings: ["transcribe documents", "digitize our collections", "improve ", "help researchers"],
+  typeSpeed: 30
+});
